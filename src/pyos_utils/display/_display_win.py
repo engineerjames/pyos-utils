e7db321ba40faf351e6ac8728678199c15f6a640
Armes, James <james.armes@medtronic.com>
2025-03-09 21:14:27 -0600

    Update windows display

--- src/pyos_utils/display/_display_win.py
+++ src/pyos_utils/display/_display_win.py
@@ -17,8 +17,7 @@ class WindowsDisplayInterface(DisplayInterface):
         return [DisplayInfo(0, 0, 0, 0)]
 
 
-# PS C:\Users\jarmes> wmic path Win32_VideoController
-# get Name,VideoModeDescription,VideoProcessor,MaxRefreshRate,MinRefreshRate,DeviceID
+# wmic path Win32_VideoController get VideoModeDescription,VideoProcessor,MaxRefreshRate,MinRefreshRate,DeviceID
 # DeviceID          MaxRefreshRate  MinRefreshRate  Name                              VideoModeDescription
 # VideoController1  143             29              NVIDIA GeForce RTX 4090           3840 x 2160 x 4294967296 colors
 # VideoController2  32              32              Microsoft Remote Display Adapter  3456 x 2158 x 4294967296 colors
