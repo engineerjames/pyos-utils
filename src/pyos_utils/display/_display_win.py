66c81b5aadb45d5cc5df9622625a29644579706a
James Armes <jamesleearmes@gmail.com>
2025-04-04 21:15:36 -0700

    Fix up Windows display management

--- src/pyos_utils/display/_display_win.py
+++ src/pyos_utils/display/_display_win.py
@@ -2,9 +2,12 @@ import subprocess
 
 from pyos_utils.display._display_info import DisplayInfo
 from pyos_utils.display._display_interface import DisplayInterface
+from pyos_utils.display._display_utilities import get_bit_depth_from_colors
 
 
 class WindowsDisplayInterface(DisplayInterface):
+    NUMBER_OF_COLUMNS = 5
+
     def get_info(self) -> list[DisplayInfo]:
         """Get the display information."""
         output = subprocess.run(
@@ -13,7 +16,8 @@ class WindowsDisplayInterface(DisplayInterface):
                 "path",
                 "Win32_VideoController",
                 "get",
-                "VideoModeDescription,VideoProcessor,MaxRefreshRate,MinRefreshRate,DeviceID",
+                "CurrentRefreshRate,DeviceID,VideoModeDescription,VideoProcessor",
+                "/format:csv",
             ],
             capture_output=True,
             text=True,
@@ -24,18 +28,32 @@ class WindowsDisplayInterface(DisplayInterface):
 
         # Extract the display information from the output
         display_info: list[DisplayInfo] = []
-        for line in output.stdout.split("\n"):
-            if line.strip():
-                parts = line.split()
-                if len(parts) >= 5:
-                    display_info.append(
-                        DisplayInfo(
-                            width=int(parts[0]),
-                            height=int(parts[1]),
-                            depth=int(parts[2]),
-                            fps=int(parts[3]),
-                        )
-                    )
+        for row in [line for line in output.stdout.split("\n") if line]:
+            # Skip the header row
+            if "DeviceID" in row:
+                continue
+
+            # Split the row into columns, and skip the first column (Node).
+            # Unsure why Powershell adds this, but it does.
+            parts = row.split(",")[1:]
+
+            fps = int(parts[0])
+            name = parts[3]
+            width = int(parts[2].split("x")[0])
+            height = int(parts[2].split("x")[1])
+            depth = int(parts[2].split("x")[2].split(" ")[1])
+
+            depth = get_bit_depth_from_colors(depth)
+
+            display_info.append(
+                DisplayInfo(
+                    name=name,
+                    width=width,
+                    height=height,
+                    depth=depth,
+                    fps=fps,
+                ),
+            )
         return display_info
 
 
