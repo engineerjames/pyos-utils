0470f0112ca4265c188286d92ac8712258f2ef1c
Armes, James <james.armes@medtronic.com>
2025-04-14 22:05:38 -0600

    Update package exports

--- src/pyos_utils/display/__init__.py
+++ src/pyos_utils/display/__init__.py
@@ -1,2 +1,11 @@
-from ._display_interface import DisplayInterface  # type: ignore[unused-import]
-from ._factory import DisplayInterfaceFactory  # type: ignore[unused-import]
+from collections.abc import Callable
+
+from ._display_interface import DisplayInterface
+from ._factory import DisplayInterfaceFactory
+
+get_display: Callable[..., DisplayInterface] = DisplayInterfaceFactory.create_interface
+
+__all__ = [
+    "DisplayInterface",
+    "get_display",
+]
